f29b21f3910d53039bd7a1db6f1de1ab69a46e65
tin <minhtien0601200@gmail.com>
2024-02-27 13:43:10 +0700

    Generic

--- src/Lambda/MyFrame.java
+++ src/Lambda/MyFrame.java
@@ -10,12 +10,16 @@ import javax.swing.JButton;
 public class MyFrame extends JFrame{
 	
 	JButton myButton = new JButton("MY BUTTON");
+	JButton myButton2 = new JButton("MY BUTTON 2");
 	
 	MyFrame(){
 		
 		myButton.setBounds(100,100,200,100);
 		myButton.addActionListener((e)->System.out.println("Tin dep trai"));
+		myButton2.setBounds(100,200,200,100);
+		myButton2.addActionListener((e)->System.out.println("Tin dep trai 2"));
 		
+		this.add(myButton2);
 		this.add(myButton);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(420, 420);
